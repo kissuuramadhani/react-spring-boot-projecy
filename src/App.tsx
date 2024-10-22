ffd87aa2f56b7ec84934286d45cac6494ed2cefc
kissuuramadhani <kissuuramadhani@gmail.com>
2024-10-22 21:01:38 +0300

    Changes added to Git

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import "./App.css";
+import { Navbar } from "./layouts/NavbarAndFooter/Navbar";
+import { Footer } from "./layouts/NavbarAndFooter/Footer";
+import { HomePage } from "./layouts/HomePage/HomePage";
+import { SearchBookPage } from "./layouts/SearchBookPage/SearchBookPage";
+import { Redirect, Route, Switch } from "react-router-dom";
+import { BookCheckoutPage } from "./layouts/BookCheckoutPage/BookCheckoutPage";
+
+export const App = () => {
+  return (
+    <div className="d-flex flex-column min-vh-100">
+      <Navbar />
+      <div className="flex-grow-1">
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to="/home" />
+          </Route>
+
+          <Route path="/home">
+            <HomePage />
+          </Route>
+
+          <Route path="/search">
+            <SearchBookPage />
+          </Route>
+
+          <Route path="/checkout/:bookId">
+            <BookCheckoutPage />
+          </Route>
+        </Switch>
+      </div>
+      <Footer />
+    </div>
+  );
+};
